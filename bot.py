504adcc56870f2fc50a8ad5006291f88de26cb78
SamirShef <ssserin10@gmail.com>
2024-06-17 18:26:35 +0300

    new commit

--- bot.py
+++ bot.py
@@ -3,26 +3,12 @@ import telebot
 
 bot = telebot.TeleBot(client.TOKEN)
 @bot.message_handler(commands=['start'])
-bot.send_message("Привет")
-def start(message):
-    # Отправляем сообщение с просьбой отправить свой ID
-    bot.send_message(message.chat.id, "Привет! Чтобы получить свой Telegram ID, нажмите на кнопку ниже.", reply_markup=types.ReplyKeyboardRemove())
-    bot.send_message(message.chat.id, "Нажмите на кнопку 👇", reply_markup=types.ForceReply())
+def send_welcome(message):
+    # Отправляем стикер
+    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAKnLWZwTe1XxbwDhMZbneoCHb_Rs_rUAAJ6AAMQIQIQAALHI6TJuQU1BA')
+    
+    # Отправляем текстовое сообщение
+    bot.send_message(message.chat.id, 'Приветственное сообщение для пользователя')
 
-@bot.message_handler(func=lambda message: True)
-def get_user_id(message):
-    # Отправляем сообщение с ID пользователя
-    bot.send_message(message.chat.id, f"Ваш Telegram ID: {message.from_user.id}")
-
-@bot.message_handler(commands=['check_subscription'])
-def check_subscription(message):
-    # Проверяем подписку пользователя на канал @businessempire_tg
-    user_id = message.from_user.id
-    is_subscribed = bot.get_chat_member('@businessempire_tg', user_id).status == 'member'
-    if is_subscribed:
-        bot.send_message(message.chat.id, "Вы подписаны на канал @businessempire_tg")
-    else:
-        bot.send_message(message.chat.id, "Вы не подписаны на канал @businessempire_tg")
-
-# Запуск бота
+# Запускаем бота
 bot.polling()
