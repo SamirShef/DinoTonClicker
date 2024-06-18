f25d2d0be272a13d9fa717872e8bc1bbb0763243
SamirShef <145897306+SamirShef@users.noreply.github.com>
2024-06-18 13:01:28 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,38 +1,27 @@
 import client
-from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+import telebot
+from telebot import types
 
-STICKER_ID = 'CAACAgIAAxkBAAKnLWZwTe1XxbwDhMZbneoCHb_Rs_rUAAJ6AAMQIQIQAALHI6TJuQU1BA'
-GAME_URL = 'https://samirshef.github.io/DinoTonClicker/'
-
-bot = Bot(client.TOKEN)
-updater = Updater(bot=bot, use_context=True)
-def start(update, context):
+bot = telebot.TeleBot(client.TOKEN)
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
     # Отправляем стикер
-    context.bot.send_sticker(chat_id=update.effective_chat.id, sticker=STICKER_ID)
+    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAKnLWZwTe1XxbwDhMZbneoCHb_Rs_rUAAJ6AAMQIQIQAALHI6TJuQU1BA')
     
-    # Отправляем приветственное сообщение
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Добро пожаловать в игру.")
+    # Отправляем текстовое сообщение
+    markup = telebot.types.InlineKeyboardMarkup()
+    play_button = types.InlineKeyboardButton(text="Играт", callback_data="play")
+    markup.add(play_button)
+    bot.send_message(message.chat.id, 'даров', reply_markup=markup)
 
-    # Создаем кнопку "Играть"
-    keyboard = [[InlineKeyboardButton("Играть", callback_data='play')]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    
-    # Отправляем сообщение с кнопкой
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Нажмите кнопку ниже, чтобы начать играть!", reply_markup=reply_markup)
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    if call.data == 'play':
+        # URL вашей игры на GitHub
+        game_url = 'https://samirshef.github.io/DinoTonClicker/'
+        bot.answer_callback_query(call.id)
+        bot.send_message(call.message.chat.id, 'Нажмите здесь, чтобы начать игру:', reply_markup=types.InlineKeyboardMarkup([[types.InlineKeyboardButton(text="Играть", url=game_url)]]))
 
-def button(update, context):
-    query = update.callback_query
-    query.answer()
-    
-    # Проверяем callback_data и открываем игру
-    if query.data == 'play':
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Открываю игру...", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Играть", url=GAME_URL)]]))
-
-# Добавляем обработчики команд
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CallbackQueryHandler(button))
 
 # Запускаем бота
-updater.start_polling()
-updater.idle()
+bot.polling()
