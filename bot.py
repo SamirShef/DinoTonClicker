2f4aa35cfa7a587eafd1242603ad17836683d870
SamirShef <145897306+SamirShef@users.noreply.github.com>
2024-06-18 15:51:31 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import client
 import telebot
 
 bot = telebot.TeleBot(client.TOKEN)
-@bot.message_handler(commands=['start'])
 def send_welcome(message):
     # Отправляем стикер
     bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAKnLWZwTe1XxbwDhMZbneoCHb_Rs_rUAAJ6AAMQIQIQAALHI6TJuQU1BA')
