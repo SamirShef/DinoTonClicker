6a642d7a530c4dae91c3565d6741f5f8f2353576
SamirShef <145897306+SamirShef@users.noreply.github.com>
2024-06-24 14:07:03 +0300

    Update DinoTonBuild.framework.js

--- Build/DinoTonBuild.framework.js
+++ Build/DinoTonBuild.framework.js
@@ -7751,7 +7751,7 @@ console.error("WebSocket error observed:", event);
 }
 
               ws.binaryType = 'arraybuffer';
-            } catch (e) 
+            )} catch (e) 
 			  {
               throw new FS.ErrnoError(23);
             }
